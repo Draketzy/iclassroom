a150f7559e533ea80a70ccf7233b3c6bde5792cb
khentapduhan20 <khentsamsonapduhan@gmail.com>
2025-06-22 20:37:31 +0800

    django

--- iClassroom/classroom/views.py
+++ iClassroom/classroom/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import TodoItem
+def home(request):
+    return render(request, "home.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": items})
 
 # Create your views here.
