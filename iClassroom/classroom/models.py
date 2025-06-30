32584fd4df9e0b4b6d6e1f30f04fb0a043f13b10
Draketzy <133954086+Draketzy@users.noreply.github.com>
2025-06-30 16:43:54 +0800

    m

--- iClassroom/classroom/models.py
+++ iClassroom/classroom/models.py
@@ -45,6 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['first_name', 'last_name']
     objects = CustomUserManager()
 
+
 # ---------------------
 # Academic Structure
 # ---------------------
